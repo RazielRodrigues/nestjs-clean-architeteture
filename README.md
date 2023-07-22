146292efe737b54c085095be24a8486894d47b5c
Raziel Rodrigues <razielx3@live.com>
2023-07-22 18:58:27 -0300

    feat: module envCOnfig

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ https://www.aluiziodeveloper.com.br/curso-de-nodejs-avan%C3%A7ado-com-clean-arch
 - nest new project
 - configurar fastify
 - tsconfig.json
+- ajustes inicio de projeto
+- variaveis de ambiente
+- nest g module shared/infraestructure/env-confi
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfigModule } from './shared/infraestructure/env-config/env-config.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, EnvConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
