3eccd9cd1a25d457ca015480f53add0115825f96
Raziel Rodrigues <razielx3@live.com>
2023-07-30 18:15:34 -0300

    test: user entity

---
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -0,0 +1,25 @@
+import { faker } from '@faker-js/faker'
+import { UserEntity, UserProps } from '../../user.entity'
+
+describe('User entity unit', () => {
+    let props: UserProps;
+    let sut: UserEntity;
+
+    beforeEach(() => {
+        props = {
+            name: faker.person.fullName(),
+            email: faker.internet.email(),
+            password: faker.internet.password()
+        }
+
+        sut = new UserEntity(props);
+    })
+
+    it('Constructor method', () => {
+        expect(sut.props.name).toEqual(props.name)
+        expect(sut.props.email).toEqual(props.email)
+        expect(sut.props.password).toEqual(props.password)
+        expect(sut.props.createdAt).toBeInstanceOf(Date)
+    })
+
+})
