4d607f4a5fdaefc42f1c3b3ddb242a92419ff69e
Raziel Rodrigues <razielx3@live.com>
2023-07-30 18:06:19 -0300

    feat: implementação dos primeiros metodos da entidade

--- dist/src/users/domain/entities/user.entity.d.ts
+++ dist/src/users/domain/entities/user.entity.d.ts
@@ -1,2 +1,10 @@
+export type UserProps = {
+    name: string;
+    email: string;
+    password: string;
+    createdAt?: Date;
+};
 export declare class UserEntity {
+    readonly props: UserProps;
+    constructor(props: UserProps);
 }
--- dist/src/users/domain/entities/user.entity.js
+++ dist/src/users/domain/entities/user.entity.js
@@ -2,6 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserEntity = void 0;
 class UserEntity {
+    constructor(props) {
+        this.props = props;
+        this.props.createdAt = this.props.createdAt ?? new Date();
+    }
 }
 exports.UserEntity = UserEntity;
 //# sourceMappingURL=user.entity.js.map
